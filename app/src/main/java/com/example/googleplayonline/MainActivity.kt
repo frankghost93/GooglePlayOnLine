1112cd5ec812b4df5bd0391d85c21262ecf1afcd
frankghost93 <liuyangghost@gmail.com>
2023-11-15 21:49:53 +0800

    专业谷歌马甲上架+tg @frankandroid 专业谷歌代上架服务-谷歌商店上架_GooglePlay上架 国内马甲上架 体育 游戏 棋牌 捕鱼 软件破解 逆向开发 破解任何软件 添加指定功能 马甲上架

---
+++ app/src/main/java/com/example/googleplayonline/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.googleplayonline
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.googleplayonline.ui.theme.GooglePlayOnLineTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            GooglePlayOnLineTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    GooglePlayOnLineTheme {
+        Greeting("Android")
+    }
+}
